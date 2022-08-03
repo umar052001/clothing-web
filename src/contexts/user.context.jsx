119372e7a858f694730c2d9b4813067875dbe111
Umar <umar.111nawaz@gmail.com>
2022-08-04 02:52:47 +0500

    setup the context for sign out

---
+++ src/contexts/user.context.jsx
@@ -0,0 +1,26 @@
+import { createContext, useEffect, useState } from "react";
+import {
+  createUserDocumentFromAuth,
+  onAuthStateChangeListner,
+} from "../utils/firebase/firebase.utils";
+
+export const UserContext = createContext({
+  currentUser: null,
+  setCurrentUser: () => null,
+});
+
+export const UserProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const value = { currentUser, setCurrentUser };
+  useEffect((user) => {
+    const unsubscribe = onAuthStateChangeListner((user) => {
+      if (user) {
+        createUserDocumentFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
+    return unsubscribe;
+  }, []);
+
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
+};
